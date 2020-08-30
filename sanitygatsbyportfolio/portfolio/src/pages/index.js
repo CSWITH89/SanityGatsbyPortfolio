83c27ee49254434196508c58bc4c7ac86accc25b
Chris Withall <chriswithall@Chriss-MacBook-Pro-2.local>
2020-08-30 13:38:41 +0100

    Initial main page styling setup

--- sanitygatsbyportfolio/portfolio/src/pages/index.js
+++ sanitygatsbyportfolio/portfolio/src/pages/index.js
@@ -1,9 +1,10 @@
 import React from "react"
 import { graphql } from "gatsby"
-
+import Img from "gatsby-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { GrLinkedin, GrYoutube, GrFacebook } from "react-icons/gr";
+import styles from './index.css';
 
 export const query = graphql`
 {
@@ -32,51 +33,50 @@ const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <section class="main-page">
+{/* SIDEBAR START */}
       <div class="sidebar">
         <div class="sidebar-content">
-          <h1 class="sidebar-content-heading">Placeholder Name</h1>
-          <ul>
-            <li>Option</li>
-            <li>Option</li>
-            <li>Option</li>
-            <li>Option</li>
-          </ul>
-          <div>
-            <p>Icon Tray placeholder</p>
-            <GrLinkedin 
-            class="icon icon-linkedin"
-            />
-            <GrYoutube
-            class="icon icon-youtube"
-             />
-            <GrFacebook
-            class="icon icon-facebook"
-            />
+          <div class="sidebar-content-header">
+            <h1 class="sidebar-content-header-text">Placeholder Name</h1>
           </div>
+          <div class="sidebar-content-navigation">
+            <ul>
+              <li>Option</li>
+              <li>Option</li>
+              <li>Option</li>
+              <li>Option</li>
+            </ul>
+          </div>
+          <footer class="sidebar-content-footer">
+            <div class="sidebar-content-footer-icons">
+              <p>Icon Tray placeholder</p>
+              <GrLinkedin 
+              class="icon icon-linkedin"
+              />
+              <GrYoutube
+              class="icon icon-youtube"
+              />
+              <GrFacebook
+              class="icon icon-facebook"
+              />
+            </div>
+          </footer>
         </div>
       </div>
-    {/* <h1>Portfolio</h1>
-    <ul style={{
-      listStyle: 'none',
-      display: 'flex',
-      alignItems: 'space-between',
-      padding: 0,
-    }}>
-      {data.allSanityProject.edges.map(({node: project})=>(
-       <li 
-       key={project.slug.current}
-       style={{ flex: '1 45%', maxWidth: '45%'}}
-       >
-         <h2
-         style={{
-           fontSize: '24px'
-         }}
-         >{project.title}</h2>
-       <Image fluid={project.screenshotImage.asset.fluid} alt={project.title} />
-      <p>{project.description}</p>
-      </li>
-      ))}
-    </ul> */}
+{/* SIDEBAR END */}
+{/* PROJECT IMAGES START */}
+<div class="project-showcase">
+  <div class="project-showcase-content">
+    {
+      data.allSanityProject.edges.map(({node: project}) => (
+        <div key={project.slug.current}>
+        <Img fluid={project.screenshotImage.asset.fluid} alt={project.title} />
+        </div>
+      ))
+    }
+  </div>
+</div>
+{/* PROJECT IMAGES END */}
     </section>
   </Layout>
 )
